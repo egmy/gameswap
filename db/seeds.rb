7abbe38ab2b4e69264ac6912452756e13c68a7c2
Brandon Hobson <brandonhobson22@gmail.com>
2022-11-10 14:50:54 -0600

    seeded 25 games with images

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,14 +9,157 @@ alice = User.create!(
 
 # games
 
-nba2k = Game.create!(
-    title: 'NBA 2K23',
-    description: "This is a basketball game",
-    image: 'NBA 2k23.png'
+advancedWarfare = Game.create!(
+    title: 'Call of Duty: Advanced Warfare',
+    description: 'Call of Duty®: Advanced Warfare, developed by Sledgehammer Games (co-developers of Call of Duty®: Modern Warfare® 3), harnesses the first three-year, all next-gen development cycle in franchise history. Call of Duty®: Advanced Warfare envisions the powerful battlegrounds of the future, where both technology and tactic have evolved to usher in a new era of combat for the franchise. Delivering a stunning performance, Academy Award® winning actor Kevin Spacey stars as Jonathan Irons – one of the most powerful men in the world – shaping this chilling vision of the future of war.',
+    image: 'Call of Duty Advanced Warfare'
+)
+
+blackOps4 = Game.create!(
+    title: 'Call of Duty: Black Ops 4',
+    description: 'Black Ops is back! Featuring gritty, grounded Multiplayer combat, the biggest Zombies offering ever with three full undead adventures at launch, and Blackout, where the universe of Black Ops comes to life in a massive battle royale experience.',
+    image: 'Call of Duty Black Ops 4.png'
+)
+
+blackOps3 = Game.create!(
+    title: 'Call of Duty: Black Ops 3',
+    description: 'Welcome to Call of Duty: Black Ops III, a dark, twisted future where a new breed of Black Ops soldier emerges and the lines are blurred between our own humanity and the technology we created to stay ahead, in a world where cutting-edge military robotics define warfare.',
+    image: 'Call of Duty Black Ops 3.png'
+)
+
+coldWar = Game.create!(
+    title: 'Call of Duty: Black Ops Cold War',
+    description: 'CoD Black Ops Cold War will drop fans into the depths of the Cold War’s volatile geopolitical battle of the early 1980s. Nothing is ever as it seems in a gripping single-player Campaign, where players will come face-to-face with historical figures and hard truths, as they battle around the globe through iconic locales like East Berlin, Vietnam, Turkey, Soviet KGB headquarters and more.',
+    image: 'Call of Duty Black Ops Cold War.png'
+)
+
+advancedWarfare = Game.create!(
+    title: 'Call of Duty: Modern Warfare',
+    description: 'Call of Duty: Advanced Warfare envisions the powerful battlegrounds of the future, where both technology and tactic have evolved to usher in a new era of combat for the franchise. Delivering a stunning performance, Academy Award® winning actor Kevin Spacey stars as Jonathan Irons – one of the most powerful men in the world – shaping this chilling vision of the future of war.',
+    image: 'Call of Duty Modern Warfare.png'
+)
+
+destiny2 = Game.create!(
+    title: 'Destiny 2',
+    description: "Dive into the world of Destiny 2 to explore the mysteries of the solar system and experience responsive first-person shooter combat. Unlock powerful elemental abilities and collect unique gear to customize your Guardian's look and playstyle. Enjoy Destiny 2’s cinematic story, challenging co-op missions, and a variety of PvP modes alone or with friends.",
+    image: 'Destiny 2.png'
+)
+
+destiny = Game.create!(
+    title: 'Destiny',
+    description: 'Destiny is a next generation first person shooter featuring an unprecedented combination of cinematic storytelling, cooperative, competitive, and public gameplay, and personal activities that are woven into an expansive, persistent online world. Venture out alone or join up with friends. The choice is yours.',
+    image: 'Destiny.png'
+)
+
+forza2 = Game.create!(
+    title: 'Forza Horizon 2',
+    description: 'A world of beauty and freedom awaits in Forza Horizon 2! Explore a beautiful open world with dynamic day, night and spectacular weather effects in stunning 1080p. Drive 200+ cars in epic road trips through a massive, wide-open world of freedom.',
+    image: 'Forza Horizon 2.png'
+)
+
+forza3 = Game.create!(
+    title: 'Forza Horizon 3',
+    description: "Star in thrilling Showcase events that pit you and your car against a fleet of speed boats, even a giant airship! HIRE AND FIRE FRIENDS You're the boss. Hire your friends' Drivatars to help you gain fans and expand your festival, and form Convoys with them to explore the world even when they're not online.",
+    image: 'Forza Horizon 3.png'
+)
+
+forza4 = Game.create!(
+    title: 'Forza Horizon 4',
+    description: 'Go it alone or team up with others to explore beautiful and historic Britain in a shared open world. Collect, modify and drive over 450 cars. Race, stunt, create and explore – choose your own path to become a Horizon Superstar. ',
+    image: 'Forza Horizon 4.png'
+)
+
+forza5 = Game.create!(
+    title: 'Forza Motorsport 5',
+    description: "Forza Motorsport 5 is a cinematic automotive journey starring the world's greatest cars and tracks. Built from the ground up to take advantage of Xbox One and the vast power of the cloud, no game better delivers the wide-eyed thrill of racing.",
+    image: 'Forza Motorsport 5.png'
+)
+
+gta5 = Game.create!(
+    title: 'Grand Theft Auto 5',
+    description: 'Pull off daring co-operative Heists, enter adrenaline-fueled Stunt Races, compete in unique Adversary Modes, or hang out in social spaces including nightclubs, arcades, penthouse parties, car meetups, and much more.',
+    image: 'Grand Theft Auto 5.png'
+)
+
+halo5 = Game.create!(
+    title: 'Halo 5 Guardians',
+    description: 'Halo 5: Guardians delivers epic multiplayer experiences that span multiple modes, full-featured level building tools, and a new chapter in the Master Chief saga. Play with friends and compete against rivals in three massive multiplayer modes new to the Halo franchise: Warzone, Warzone Assault, and Warzone Firefight.',
+    image: 'Halo 5 Guardians.png'
+)
+
+madden20 = Game.create!(
+    title: 'Madden NFL 20',
+    description: "Be the Face of an NFL franchise where the decisions you make matter in your journey to become an NFL Superstar in 'FACE OF THE FRANCHISE: QB1'. Feel the emotion, personality, and power of NFL Superstars with 'SUPERSTAR XFACTOR', an all-new abilities progression system that reveals special abilities for today's most exciting NFL Superstars heightening the level strategy and excitement in every game.",
+    image: 'Madden NFL 20.png'
+)
+
+madden21 = Game.create!(
+    title: 'Madden NFL 21',
+    description: 'Madden NFL 21 delivers innovation including: Face of the franchise: Rise to fame: Rise to fame and become immortalized in Madden NFL 21 as you transcend NFL history and shape your path to greatness. Take control: Master all-new running enhancements and live playbooks to drive up the score in Madden NFL 21.',
+    image: 'Madden NFL 21.png'
 )
 
 madden22 = Game.create!(
     title: 'Madden NFL 22',
-    description: 'This is a football game',
+    description: 'Take down the competition, hone your skills, and build your fantasy roster with Madden 22 Face of the Franchise, The Yard, and Ultimate Team in this highly-anticipated NFL football video game.',
     image: 'Madden NFL 22.png'
+)
+
+madden23 = Game.create!(
+    title: 'Madden NFL 23',
+    description: 'Control your impact with every decision in all-new ways. Call the shots in Franchise with free agency and trade logic updates, play your way into the history books in Face of the Franchise: The League, and assemble the most powerful roster in all of Madden Ultimate Team™.',
+    image: 'Madden NFL 23.png'
+)
+
+mortalKombat10 = Game.create!(
+    title: 'Mortal Kombat 10',
+    description: 'Who’s Next? Experience the Next Generation of the #1 Fighting Franchise.
+
+    Mortal Kombat X combines unparalleled, cinematic presentation with all new gameplay. For the first time, players can choose from multiple variations of each character impacting both strategy and fighting style.Who’s Next? Experience the Next Generation of the #1 Fighting Franchise.',
+    image: 'Mortal Kombat 10.png'
+)
+
+mortalKombat11 = Game.create!(
+    title: 'Mortal Kombat 11',
+    description: "Mortal Kombat is back and better than ever in the next evolution of the iconic franchise. The all new Custom Character Variations give you unprecedented control to customize the fighters and make them your own. The new graphics engine showcasing every skull-shattering, eye-popping moment, brings you so close to the fight you can feel it. And featuring a roster of new and returning Klassic Fighters, Mortal Kombat's best in class cinematic story mode continues the epic saga over 25 years in the making.",
+    image: 'Mortal Kombat 11.png'
+)
+
+nba2k20 = Game.create!(
+    title: 'NBA 2K20',
+    description: "NBA 2K has evolved into much more than a basketball simulation. 2K continues to redefine what's possible in sports gaming with NBA 2K20, featuring best in class graphics & gameplay, ground breaking game modes, and unparalleled player control and customization. Plus, with its immersive open-world Neighborhood, NBA 2K20 is a platform for gamers and ballers to come together and create what's next in basketball culture.",
+    image: 'NBA 2K20.png'
+)
+
+nba2k21 = Game.create!(
+    title: 'NBA 2K21',
+    description: "NBA 2K21 is the latest release in the world-renowned, best-selling NBA 2K series. 2K21 leads the charge with next-gen innovations, while continuing to deliver an industry-leading sports video game experience on the current generation of gaming platforms. With extensive improvements upon its best-in-class graphics and gameplay, competitive and community online features, and deep, varied game modes, NBA 2K21 offers one-of-a-kind immersion into all facets of NBA basketball and culture - where Everything is Game.",
+    image: 'NBA 2K21.png'
+)
+
+nba2k22 = Game.create!(
+    title: 'NBA 2K22',
+    description: "A WORLD OF BASKETBALL. NBA 2K22 puts the entire basketball universe in your hands. PLAY NOW in real NBA and WNBA environments against authentic teams and players. Build your own dream team in MyTEAM with today's stars and yesterday's legends. Live out your own pro journey in MyCAREER and experience your personal rise to the NBA. Flex your management skills as a powerful Executive in MyGM and MyLEAGUE. Anyone, anywhere can hoop in NBA 2K22.",
+    image: 'NBA 2K22.png'
+)
+
+
+nba2k23 = Game.create!(
+    title: 'NBA 2K23',
+    description: "Become the MVP of the league with NBA 2K23 on Xbox One! Play as some of the biggest names in the NBA or write your own legacy in MyCAREER. Redefine the game as you put together your dream team in MyTEAM and develop your skills on the court with authentic gameplay in this iconic basketball video game.",
+    image: 'NBA 2K23.png'
+)
+
+redDead2 = Game.create!(
+    title: 'Red Dead Redemption 2',
+    description: "The end of the Wild West era has begun. After a robbery gone wrong in the western town of Blackwater, Arthur Morgan and the Van der Linde gang are forced to flee. With federal agents and the best bounty hunters in the nation massing on their heels, the gang must rob, steal and fight their way across the rugged heartland of America in order to survive. As deepening internal divisions threaten to tear the gang apart, Arthur must make a choice between his own ideals and loyalty to the gang who raised him.
+
+    Now featuring additional Story Mode content and a fully-featured Photo Mode, Red Dead Redemption 2 also includes free access to the shared living world of Red Dead Online, where players take on an array of roles to carve their own unique path on the frontier as they track wanted criminals as a Bounty Hunter, create a business as a Trader, unearth exotic treasures as a Collector or run an underground distillery as a Moonshiner and much more.",
+    image: 'Red Dead Redemption 2.png'
+)
+
+rocketLeague = Game.create!(
+    title: 'Rocket League',
+    description: "Rocket League is futuristic Sports-Action game, in which players are equipped with booster-rigged vehicles that can be crashed into balls for incredible goals or epic saves across multiple, highly-detailed arenas. Using an advanced physics system to simulate realistic interactions, Rocket League relies on mass and momentum to give players a complete sense of intuitive control in this unbelievable, high-octane re-imagining of association football.",
+    image: 'Rocket League.png'
 )
