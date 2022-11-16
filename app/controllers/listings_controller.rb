921120cdaf6768acdb1970e6d32cb9728a08d5fe
egmy <ericgraymyers012@gmail.com>
2022-11-16 13:20:03 -0600

    changed error

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,2 +1,15 @@
 class ListingsController < ApplicationController
+    def index
+        @game = Game.find(params[:game_id])
+        @listings = @game.listings
+        render :index
+
+      end
+
+
+      def new
+        @game = Game.find(params[:game_id])
+        @listing = Listing.new
+        render :new
+      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   root to: redirect('index')
 
   get 'index', to: 'games#index', as: 'games'
+
+  get 'index/:game_id', to: 'listings#index', as: 'game'
+  
+  get 'index/:game_id/new', to: 'listings#new', as: 'new_game_listing'
+
 end
