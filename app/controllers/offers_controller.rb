4ccc2ff005bbbfa975feba10176ec154ab49030f
Elliott Schwarzman <nschwrzm@memphis.edu>
2022-11-16 16:43:30 -0600

    route, new action added, initial progress to new view

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,2 +1,8 @@
 class OffersController < ApplicationController
+
+  def new
+    @listing = Listing.find(params[:listing_id])
+    @offer = Offer.new
+    render :new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   get 'index', to: 'games#index', as: 'games'
 
   get 'index/:game_id', to: 'listings#index', as: 'game'
-  
+
   get 'index/:game_id/new', to: 'listings#new', as: 'new_game_listing'
+
+  get 'listing/:listing_id/offer/new', to: 'offers#new', as: 'new_offer'
 
 end
