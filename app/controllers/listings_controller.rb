40b910cc70ae06841449b56317bee2b7ffc4ed15
Brandon Hobson <brandonhobson22@gmail.com>
2022-11-17 16:27:55 -0600

    Added user authentication and views

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,15 +1,17 @@
 class ListingsController < ApplicationController
-    def index
-        @game = Game.find(params[:game_id])
-        @listings = @game.listings
-        render :index
+  before_action :authenticate_user!, except: [:index]
+  
+  def index
+      @game = Game.find(params[:game_id])
+      @listings = @game.listings
+      render :index
 
-      end
+    end
 
 
-      def new
-        @game = Game.find(params[:game_id])
-        @listing = Listing.new
-        render :new
-      end
+    def new
+      @game = Game.find(params[:game_id])
+      @listing = Listing.new
+      render :new
+    end
 end
