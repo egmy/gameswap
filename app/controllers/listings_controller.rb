596a97e5bd3a3d1d1152da276a8c61f005fff795
egmy <ericgraymyers012@gmail.com>
2022-11-17 17:03:19 -0600

    working

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,4 +1,5 @@
 class ListingsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
     def index
         @game = Game.find(params[:game_id])
         @listings = @game.listings
@@ -8,8 +9,20 @@ class ListingsController < ApplicationController
 
 
       def new
-        @game = Game.find(params[:game_id])
-        @listing = Listing.new
+        @game = Game.find(params[:game_id]) 
+        @listing=Listing.new
         render :new
+      end
+
+      def create
+        @game = Game.find(params[:game_id])
+        @listing= @game.listings.build(params.require(:listing).permit(:condition, :description))
+        if @listing.save
+          flash[:success] = "Listing saved successfully"
+          redirect_to game_url(@game)
+        else
+          flash.now[:error] = "Listing could not be saved"
+          render :new, status: :unprocessable_entity
+        end
       end
 end
