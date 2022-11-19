37b3becb82ff147b64ba37e09feb3708826bb4e4
egmy <ericgraymyers012@gmail.com>
2022-11-18 19:21:56 -0600

    changed links/format

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,29 +1,28 @@
 class ListingsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
     def index
-        @game = Game.find(params[:game_id])
-        @listings = @game.listings
-        render :index
+      @game = Game.find(params[:game_id])
+      @listings = @game.listings
+      render :index
+    end
 
-      end
 
+    def new
+      @game = Game.find(params[:game_id]) 
+      @listing = Listing.new
+      render :new
+    end
 
-      def new
-        @game = Game.find(params[:game_id]) 
-        @listing=Listing.new
-        render :new
-      end
-
-      def create
-        @game = Game.find(params[:game_id])
-        @listing= @game.listings.build(params.require(:listing).permit(:condition, :description, :user_id))
-        @listing.user=current_user
-        if @listing.save!
+    def create
+      @game = Game.find(params[:game_id])
+      @listing= @game.listings.build(params.require(:listing).permit(:condition, :description, :user_id))
+      @listing.user=current_user
+      if @listing.save!
           flash[:success] = "Listing saved successfully"
           redirect_to game_url(@game)
-        else
+      else
           flash.now[:error] = "Listing could not be saved"
           render :new, status: :unprocessable_entity
         end
-      end
+    end
 end
