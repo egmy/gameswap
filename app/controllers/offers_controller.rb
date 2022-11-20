838fcebf042d2712af9fe07c1727790a2c0eaacd
Elliott Schwarzman <nschwrzm@memphis.edu>
2022-11-20 00:15:53 -0600

    create action now functional, devise permissions added to offers actions

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,13 +1,14 @@
 class OffersController < ApplicationController
   before_action :authenticate_user!, except: [:games_index]
-  # before_action :require_permission, except: [:games_index, :new]
+  before_action :require_permission, except: [:games_index, :new]
 
-  # def require_permission
-  #   if Offer.find(params[:id]).creator != current_user
-  #     flash[:error] = "You do not have permission to do that"
-  #     redirect_to game_path(Listing.find(params[:listing_id]).game_id)
-  #   end
-  # end
+  def require_permission
+    @listing = Listing.find(params[:listing_id]) # do not allow Listing owners to make offers on their own listings
+    if @listing.user == current_user # user, not owner
+      flash[:error] = "You do not have permission to do that"
+      redirect_to game_path(Listing.find(params[:listing_id]).game_id)
+    end
+  end
 
   # disallow rendering this page if it listing_id doesn't exist/wasn't accessed via link on offers/new.html.erb
   def games_index # used for WF06, 'Choose from a list' possibly move route, action to another controller
@@ -18,33 +19,20 @@ class OffersController < ApplicationController
   def new
     @listing = Listing.find(params[:listing_id])
     @offer = Offer.new
+    @user = current_user
     render :new
   end
 
-  # last night's attempt
-  # def create
-  #   @listing = Listing.find(params[:listing_id])
-  #   #@game_id = Game.where(title: params[:title]).ids
-  #   @offer = Offer.new(params.require(:offer).permit(:description, :condition, :listing_id, :game_id => Game.where(title: :title)))#, :listing_id)) # last 2 are placeholders
-  #   #@offer = current_user.offers.build(params.require(:offer).permit(:title, :description, :condition, :listing_id, @listing.game_id))
-  #   if @offer.save
-  #     flash[:success] = "New offer successfully created"
-  #     redirect_to game_path(@listing.game_id)
-  #   else
-  #     flash.now[:error] = "An error occurred during offer creation"
-  #     @offer.errors.full_messages
-  #     render :new, status: :unprocessable_entity
-  #   end
-  # end
-
-  # Not yet functional
   def create
     @listing = Listing.find(params[:listing_id])
-    # game_id = Game.where(params.permit[:title]).ids[0] # variations of this cause another unpermitted parameter error
-    #@game = Game.where(params[:title])
-    @game = Game.find(params[:offer][:game_id])
-
-    @offer = current_user.offers.build(params.require(:offer).permit(:condition, :description, @listing, status: "active")) # tried permitting :title here, also assigning @game_id => Game.where(params.permit[:title]), and many other variations
+    @game = Game.where(title: params[:title])[0] # must use first index because .where() returns an array of all matching games; there should be only one
+    @offer = current_user.offers.build(params.require(:offer).permit(:condition, :description)) # tried permitting :title here, also assigning @game_id => Game.where(params.permit[:title]), and many other variations
+    if @game != nil # @game, therefore game_id must exist in order to save it; if this fails, @offer.save will fail and flash an error
+      @offer.game_id = @game.id
+    end
+    @offer.listing_id = @listing.id
+    @offer.status = "active" # required
+    # note that accepted attribute is initially nil
     if @offer.save
       flash[:success] = "Offer successfully created"
       redirect_to games_path()
